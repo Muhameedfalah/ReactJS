caef7879289c238fb00f1bc66284334f5316127f
Muhameedfalah <146132773+Muhameedfalah@users.noreply.github.com>
2024-06-23 14:26:20 +0300

    Delete employee-search-app directory

--- employee-search-app/src/pages/NotFoundPage.jsx
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-  return (
-    <div className="w-full flex items-center justify-center pt-20">
-      <h1 className="text-red-500 font-bold text-4xl">404 Not Found</h1>
-    </div>
-  )
-}
-
-export default NotFoundPage
